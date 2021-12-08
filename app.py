341742b4017a119b7896adba9f22acb8b438b7cb
Xin Jin <jinxin8295@gmail.com>
2021-12-08 08:40:33 -0500

    falsk added

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import pickle
 import pandas as pd
 from skimage import io
 from skimage import transform
+import numpy as np
+from flask import send_from_directory, Flask, request, jsonify, render_template, redirect, url_for
+
 
 
 app = flask.Flask(__name__, template_folder='templates')
@@ -11,6 +14,7 @@ app = flask.Flask(__name__, template_folder='templates')
 path_to_vectorizer = 'models/vectorizer.pkl'
 path_to_text_classifier = 'models/text-classifier.pkl'
 path_to_image_classifier = 'models/image-classifier.pkl'
+path_to_loan_prediction = 'models/loanPrediction.pkl'
 
 with open(path_to_vectorizer, 'rb') as f:
     vectorizer = pickle.load(f)
@@ -21,6 +25,8 @@ with open(path_to_text_classifier, 'rb') as f:
 with open(path_to_image_classifier, 'rb') as f:
     image_classifier = pickle.load(f)
 
+with open(path_to_loan_prediction, 'rb') as f:
+    loan_prediction = pickle.load(f)
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
@@ -31,35 +37,80 @@ def main():
 
     if flask.request.method == 'POST':
         # Get the input from the user.
-        user_input_text = flask.request.form['user_input_text']
+        # user_input_text = flask.request.form['user_input_text']
+
+
+        uer_contractType=flask.request.form['loan_type']               
+        user_credit=flask.request.form['ask_loan_amount']                                
+        user_downPayment=flask.request.form['down_payment_amount']           
+        user_goodsPrice=flask.request.form['goods_price']              
+        user_weekdate=flask.request.form['date']
+        user_hour=flask.request.form['time_hour']     
+        user_accompany=flask.request.form['accompany']             
+        user_type=flask.request.form['client_type']       
+        user_goodsCategory=flask.request.form['goods_category']         
+        user_sellerIndustry=flask.request.form['seller_industry']
+        user_loanTerm=flask.request.form['payment_term']                  
+        user_gender=flask.request.form['user_gender']        
+        user_ownCar=flask.request.form['user_ownCar']       
+        user_ownHouse=flask.request.form['user_haveHouse']              
+        user_childrenCount=flask.request.form['user_childrenCount']                  
+        user_annualIncome=flask.request.form['user_annualIncome']              
+        user_incomeType=flask.request.form['user_incomeType']              
+        user_educationType=flask.request.form['user_educationType']          
+        user_familyStatus=flask.request.form['user_familyStatus']           
+        user_housingType=flask.request.form['user_houseType']            
+        user_daysBirth=flask.request.form['user_age']                
+        user_daysEmployed=flask.request.form['User_employment_days']                 
+        user_ownCarAge=flask.request.form['User_ageOwnCar']                 
+        user_ownMobil=flask.request.form['user_ownMobile']                            
+        user_ownEmail=flask.request.form['user_ownEmail']                    
+        user_familyMemberCount=flask.request.form['User_familyMembersCount']   
+
+
+
+        list_of_inputs=[uer_contractType,user_credit,user_downPayment,user_goodsPrice,user_weekdate,user_hour,user_accompany,user_type,
+        user_goodsCategory,user_sellerIndustry,user_loanTerm,user_gender,user_ownCar,user_ownHouse,user_childrenCount,user_annualIncome,
+        user_incomeType,user_educationType,user_familyStatus,user_housingType,user_daysBirth,user_daysEmployed,user_ownCarAge,user_ownMobil,
+        user_ownEmail,user_familyMemberCount]
+
+        result = loan_prediction(list_of_inputs)
         
-        # Turn the text into numbers using our vectorizer
-        X = vectorizer.transform([user_input_text])
+        if result=='Approved':
+            prediction='Approved'
+        else:
+            prediction='Not Approved'
+            
+        return flask.render_template('index.html',
+            result=prediction)
         
-        # Make a prediction 
-        predictions = model.predict(X)
+        # # Turn the text into numbers using our vectorizer
+        # X = vectorizer.transform([user_input_text])
         
-        # Get the first and only value of the prediction.
-        prediction = predictions[0]
+        # # Make a prediction 
+        # predictions = model.predict(X)
+        
+        # # Get the first and only value of the prediction.
+        # prediction = predictions[0]
 
-        # Get the predicted probabs
-        predicted_probas = model.predict_proba(X)
+        # # Get the predicted probabs
+        # predicted_probas = model.predict_proba(X)
 
-        # Get the value of the first, and only, predicted proba.
-        predicted_proba = predicted_probas[0]
+        # # Get the value of the first, and only, predicted proba.
+        # predicted_proba = predicted_probas[0]
 
-        # The first element in the predicted probabs is % democrat
-        precent_democrat = predicted_proba[0]
+        # # The first element in the predicted probabs is % democrat
+        # precent_democrat = predicted_proba[0]
 
-        # The second elemnt in predicted probas is % republican
-        precent_republican = predicted_proba[1]
+        # # The second elemnt in predicted probas is % republican
+        # precent_republican = predicted_proba[1]
 
 
-        return flask.render_template('index.html', 
-            input_text=user_input_text,
-            result=prediction,
-            precent_democrat=precent_democrat,
-            precent_republican=precent_republican)
+        # return flask.render_template('index.html', 
+        #     input_text=user_input_text,
+        #     result=prediction,
+        #     precent_democrat=precent_democrat,
+        #     precent_republican=precent_republican)
 
 
 
--- create_model.py
+++ create_model.py
@@ -1,33 +1,221 @@
 import pandas as pd
 import pickle
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.naive_bayes import MultinomialNB
+# from sklearn.feature_extraction.text import TfidfVectorizer
+# from sklearn.naive_bayes import MultinomialNB
 
+# Import our libraries 
 
-df = pd.read_csv('https://raw.githubusercontent.com/CUNYTechPrep/2020-fall-data-science/master/Week7-NLP/data/dem-vs-rep-tweets.csv')
+# Pandas and numpy for data wrangling
+import numpy as np
+
+# Seaborn / matplotlib for visualization 
+import seaborn as sns
+sns.set()
+
+# Import the trees from sklearn
+from sklearn import tree
+
+# Helper function to split our data
+from sklearn.model_selection import train_test_split
+
+# Helper fuctions to evaluate our model.
+from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix, f1_score, roc_auc_score 
+
+# Helper function for hyper-parameter turning.
+from sklearn.model_selection import GridSearchCV
+
+# Import our Decision Tree
+from sklearn.tree import DecisionTreeClassifier 
+
+# Import our Random Forest 
+from sklearn.ensemble import RandomForestClassifier
+
+# Library for visualizing our tree
+# If you get an error, run 'conda install python-graphviz' in your terminal
+import graphviz
+
+from sklearn.tree import DecisionTreeClassifier 
+
+df = pd.read_csv('https://raw.githubusercontent.com/Project Dataset/Application_Data.csv')
 
 # Define our X
-X = df['Tweet']
+# uer_contractType=df['NAME_CONTRACT_TYPE']               
+# user_credit=df['AMT_APPLICATION']                                
+# user_downPayment=df['AMT_DOWN_PAYMENT']           
+# user_goodsPrice=df['AMT_GOODS_PRICE']               
+# user_weekdate=df['WEEKDAY_APPR_PROCESS_START']
+# user_hour=df['HOUR_APPR_PROCESS_START']      
+# user_accompany=df['NAME_TYPE_SUITE']              
+# user_type=df['NAME_CLIENT_TYPE']         
+# user_goodsCategory=df['NAME_GOODS_CATEGORY']         
+# user_sellerIndustry=df['NAME_SELLER_INDUSTRY']
+# user_loanTerm=df['CNT_PAYMENT']                   
+# user_gender=df['CODE_GENDER']        
+# user_ownCar=df['FLAG_OWN_CAR']          
+# user_ownHouse=df['FLAG_OWN_REALTY']               
+# user_childrenCount=df['CNT_CHILDREN']                  
+# user_annualIncome=df['AMT_INCOME_TOTAL']              
+# user_incomeType=df['NAME_INCOME_TYPE']              
+# user_educationType=df['NAME_EDUCATION_TYPE']           
+# user_familyStatus=df['NAME_FAMILY_STATUS']            
+# user_housingType=df['NAME_HOUSING_TYPE']             
+# user_daysBirth=df['DAYS_BIRTH']                    
+# user_daysEmployed=df['DAYS_EMPLOYED']                 
+# user_ownCarAge=df['OWN_CAR_AGE']                  
+# user_ownMobil=df['FLAG_MOBIL']                             
+# user_ownEmail=df['FLAG_EMAIL']                    
+# user_familyMemberCount=df['CNT_FAM_MEMBERS']     
 
-# Define our y
-y = df['Party']
+df['NAME_CONTRACT_STATUS']=df['NAME_CONTRACT_STATUS'].replace('Unused offer', 'Approved')
+df.drop(df[df['NAME_CONTRACT_STATUS'] =='Canceled'].index, inplace = True)
 
-# Initalize our vectorizer
-vectorizer = TfidfVectorizer()
+df = df.dropna( how='any',subset=['AMT_CREDIT'])
 
-# Fit our vectorizer
-vectorizer.fit(X)
+df = df.dropna( how='any',subset=['CODE_GENDER'])
 
-# Transform your X data using your fitted vectorizer. 
-X = vectorizer.transform(X)
+df = df.dropna( how='any',subset=['AMT_GOODS_PRICE'])
 
-model = MultinomialNB(alpha=.025)
+df = df.dropna( how='any',subset=['CNT_PAYMENT'])
 
-# Fit our model with our training data.
+df['AMT_DOWN_PAYMENT'] = np.where(((df['AMT_DOWN_PAYMENT'].isnull()==True) ), 0,df['AMT_DOWN_PAYMENT'] )
+
+df['OWN_CAR_AGE'] = np.where(((df['OWN_CAR_AGE'].isnull()==True) ), 0,df['OWN_CAR_AGE'] )
+
+df['CNT_FAM_MEMBERS'] = np.where(((df['CNT_FAM_MEMBERS'].isnull()==True) ), 0,df['CNT_FAM_MEMBERS'] )
+
+df['NAME_TYPE_SUITE'] = np.where(((df['NAME_TYPE_SUITE'].isnull()==True) ), 'No Specified',df['NAME_TYPE_SUITE'] )
+
+df['CODE_GENDER'] = np.where(((df['CODE_GENDER']=='M') ), '1','0')
+
+df['FLAG_OWN_CAR'] = np.where(((df['FLAG_OWN_CAR']=='Y') ), '1','0')
+
+df['FLAG_OWN_REALTY'] = np.where(((df['FLAG_OWN_REALTY']=='Y') ), '1','0')
+
+df = pd.get_dummies(df, columns=['NAME_CONTRACT_TYPE','WEEKDAY_APPR_PROCESS_START','NAME_TYPE_SUITE',
+                                'NAME_CLIENT_TYPE','NAME_GOODS_CATEGORY','NAME_SELLER_INDUSTRY','NAME_INCOME_TYPE',
+                                 'NAME_EDUCATION_TYPE','NAME_FAMILY_STATUS','NAME_HOUSING_TYPE','NAME_YIELD_GROUP'
+                                ], drop_first=True)
+
+#prepare datas for build
+selected_features = ['AMT_APPLICATION',
+'AMT_DOWN_PAYMENT',
+'AMT_GOODS_PRICE',
+'HOUR_APPR_PROCESS_START',
+'NAME_CONTRACT_STATUS',
+'CNT_PAYMENT',
+'CODE_GENDER',
+'FLAG_OWN_CAR',
+'FLAG_OWN_REALTY',
+'CNT_CHILDREN',
+'AMT_INCOME_TOTAL',
+'DAYS_BIRTH',
+'DAYS_EMPLOYED',
+'OWN_CAR_AGE',
+'FLAG_MOBIL',
+'FLAG_EMAIL',
+'CNT_FAM_MEMBERS',
+'NAME_CONTRACT_TYPE_Consumer loans',
+'NAME_CONTRACT_TYPE_Revolving loans',
+'WEEKDAY_APPR_PROCESS_START_MONDAY',
+'WEEKDAY_APPR_PROCESS_START_SATURDAY',
+'WEEKDAY_APPR_PROCESS_START_SUNDAY',
+'WEEKDAY_APPR_PROCESS_START_THURSDAY',
+'WEEKDAY_APPR_PROCESS_START_TUESDAY',
+'WEEKDAY_APPR_PROCESS_START_WEDNESDAY',
+'NAME_TYPE_SUITE_Family',
+'NAME_TYPE_SUITE_Group of people',
+'NAME_TYPE_SUITE_No Specified',
+'NAME_TYPE_SUITE_Other_A',
+'NAME_TYPE_SUITE_Other_B',
+'NAME_TYPE_SUITE_Spouse, partner',
+'NAME_TYPE_SUITE_Unaccompanied',
+'NAME_CLIENT_TYPE_Refreshed',
+'NAME_CLIENT_TYPE_Repeater',
+'NAME_CLIENT_TYPE_XNA',
+'NAME_GOODS_CATEGORY_Animals',
+'NAME_GOODS_CATEGORY_Audio/Video',
+'NAME_GOODS_CATEGORY_Auto Accessories',
+'NAME_GOODS_CATEGORY_Clothing and Accessories',
+'NAME_GOODS_CATEGORY_Computers',
+'NAME_GOODS_CATEGORY_Construction Materials',
+'NAME_GOODS_CATEGORY_Consumer Electronics',
+'NAME_GOODS_CATEGORY_Direct Sales',
+'NAME_GOODS_CATEGORY_Education',
+'NAME_GOODS_CATEGORY_Fitness',
+'NAME_GOODS_CATEGORY_Furniture',
+'NAME_GOODS_CATEGORY_Gardening',
+'NAME_GOODS_CATEGORY_Homewares',
+'NAME_GOODS_CATEGORY_Insurance',
+'NAME_GOODS_CATEGORY_Jewelry',
+'NAME_GOODS_CATEGORY_Medical Supplies',
+'NAME_GOODS_CATEGORY_Medicine',
+'NAME_GOODS_CATEGORY_Mobile',
+'NAME_GOODS_CATEGORY_Office Appliances',
+'NAME_GOODS_CATEGORY_Other',
+'NAME_GOODS_CATEGORY_Photo / Cinema Equipment',
+'NAME_GOODS_CATEGORY_Sport and Leisure',
+'NAME_GOODS_CATEGORY_Tourism',
+'NAME_GOODS_CATEGORY_Vehicles',
+'NAME_GOODS_CATEGORY_Weapon',
+'NAME_GOODS_CATEGORY_XNA',
+'NAME_SELLER_INDUSTRY_Clothing',
+'NAME_SELLER_INDUSTRY_Connectivity',
+'NAME_SELLER_INDUSTRY_Construction',
+'NAME_SELLER_INDUSTRY_Consumer electronics',
+'NAME_SELLER_INDUSTRY_Furniture',
+'NAME_SELLER_INDUSTRY_Industry',
+'NAME_SELLER_INDUSTRY_Jewelry',
+'NAME_SELLER_INDUSTRY_MLM partners',
+'NAME_SELLER_INDUSTRY_Tourism',
+'NAME_SELLER_INDUSTRY_XNA',
+'NAME_INCOME_TYPE_Maternity leave',
+'NAME_INCOME_TYPE_Pensioner',
+'NAME_INCOME_TYPE_State servant',
+'NAME_INCOME_TYPE_Student',
+'NAME_INCOME_TYPE_Unemployed',
+'NAME_INCOME_TYPE_Working',
+'NAME_EDUCATION_TYPE_Higher education',
+'NAME_EDUCATION_TYPE_Incomplete higher',
+'NAME_EDUCATION_TYPE_Lower secondary',
+'NAME_EDUCATION_TYPE_Secondary / secondary special',
+'NAME_FAMILY_STATUS_Married',
+'NAME_FAMILY_STATUS_Separated',
+'NAME_FAMILY_STATUS_Single / not married',
+'NAME_FAMILY_STATUS_Widow',
+'NAME_HOUSING_TYPE_House / apartment',
+'NAME_HOUSING_TYPE_Municipal apartment',
+'NAME_HOUSING_TYPE_Office apartment',
+'NAME_HOUSING_TYPE_Rented apartment',
+'NAME_HOUSING_TYPE_With parents',
+'NAME_YIELD_GROUP_high',
+'NAME_YIELD_GROUP_low_action',
+'NAME_YIELD_GROUP_low_normal',
+'NAME_YIELD_GROUP_middle']
+
+X = df[selected_features]
+y = df['NAME_CONTRACT_STATUS']
+
+model = DecisionTreeClassifier(criterion = "gini", random_state = 100,
+                               max_depth=5, min_samples_leaf=5)
+
 model.fit(X, y)
+pickle.dump(model, open('models/loanPrediction.pkl', 'wb') )
+# # Initalize our vectorizer
+# DecisionTree = DecisionTreeClassifier()
 
-# Save our vectorizer and model.
-pickle.dump(vectorizer, open('models/vectorizer.pkl', 'wb') )
-pickle.dump(model, open('models/text-classifier.pkl', 'wb') )
+# # Fit our vectorizer
+# DecisionTree.fit(X)
+
+# # Transform your X data using your fitted vectorizer. 
+# X = vectorizer.transform(X)
+
+# model = MultinomialNB(alpha=.025)
+
+# # Fit our model with our training data.
+# model.fit(X, y)
+
+# # Save our vectorizer and model.
+# pickle.dump(vectorizer, open('models/vectorizer.pkl', 'wb') )
+# pickle.dump(model, open('models/text-classifier.pkl', 'wb') )
 
 
