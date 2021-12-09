27fac4c6aef1d18f417f9c4f4f2755ca1d2579e4
Xin Jin <jinxin8295@gmail.com>
2021-12-08 20:01:41 -0500

    flask working fine now

--- create_model.py
+++ create_model.py
@@ -1,70 +1,14 @@
 import pandas as pd
 import pickle
-# from sklearn.feature_extraction.text import TfidfVectorizer
-# from sklearn.naive_bayes import MultinomialNB
-
-# Import our libraries 
-
-# Pandas and numpy for data wrangling
 import numpy as np
-
-# Seaborn / matplotlib for visualization 
-import seaborn as sns
-sns.set()
-
-# Import the trees from sklearn
 from sklearn import tree
-
-# Helper function to split our data
 from sklearn.model_selection import train_test_split
-
-# Helper fuctions to evaluate our model.
 from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix, f1_score, roc_auc_score 
-
-# Helper function for hyper-parameter turning.
 from sklearn.model_selection import GridSearchCV
-
-# Import our Decision Tree
 from sklearn.tree import DecisionTreeClassifier 
-
-# Import our Random Forest 
 from sklearn.ensemble import RandomForestClassifier
 
-# Library for visualizing our tree
-# If you get an error, run 'conda install python-graphviz' in your terminal
-import graphviz
-
-from sklearn.tree import DecisionTreeClassifier 
-
-df = pd.read_csv('https://raw.githubusercontent.com/Project Dataset/Application_Data.csv')
-
-# Define our X
-# uer_contractType=df['NAME_CONTRACT_TYPE']               
-# user_credit=df['AMT_APPLICATION']                                
-# user_downPayment=df['AMT_DOWN_PAYMENT']           
-# user_goodsPrice=df['AMT_GOODS_PRICE']               
-# user_weekdate=df['WEEKDAY_APPR_PROCESS_START']
-# user_hour=df['HOUR_APPR_PROCESS_START']      
-# user_accompany=df['NAME_TYPE_SUITE']              
-# user_type=df['NAME_CLIENT_TYPE']         
-# user_goodsCategory=df['NAME_GOODS_CATEGORY']         
-# user_sellerIndustry=df['NAME_SELLER_INDUSTRY']
-# user_loanTerm=df['CNT_PAYMENT']                   
-# user_gender=df['CODE_GENDER']        
-# user_ownCar=df['FLAG_OWN_CAR']          
-# user_ownHouse=df['FLAG_OWN_REALTY']               
-# user_childrenCount=df['CNT_CHILDREN']                  
-# user_annualIncome=df['AMT_INCOME_TOTAL']              
-# user_incomeType=df['NAME_INCOME_TYPE']              
-# user_educationType=df['NAME_EDUCATION_TYPE']           
-# user_familyStatus=df['NAME_FAMILY_STATUS']            
-# user_housingType=df['NAME_HOUSING_TYPE']             
-# user_daysBirth=df['DAYS_BIRTH']                    
-# user_daysEmployed=df['DAYS_EMPLOYED']                 
-# user_ownCarAge=df['OWN_CAR_AGE']                  
-# user_ownMobil=df['FLAG_MOBIL']                             
-# user_ownEmail=df['FLAG_EMAIL']                    
-# user_familyMemberCount=df['CNT_FAM_MEMBERS']     
+df = pd.read_csv('https://raw.githubusercontent.com/DataScience_Project/Project_Dataset/Application_Data.csv')
 
 df['NAME_CONTRACT_STATUS']=df['NAME_CONTRACT_STATUS'].replace('Unused offer', 'Approved')
 df.drop(df[df['NAME_CONTRACT_STATUS'] =='Canceled'].index, inplace = True)
@@ -76,6 +20,8 @@ df = df.dropna( how='any',subset=['CODE_GENDER'])
 df = df.dropna( how='any',subset=['AMT_GOODS_PRICE'])
 
 df = df.dropna( how='any',subset=['CNT_PAYMENT'])
+
+df = df.dropna( how='any',subset=['CODE_GENDER'])
 
 df['AMT_DOWN_PAYMENT'] = np.where(((df['AMT_DOWN_PAYMENT'].isnull()==True) ), 0,df['AMT_DOWN_PAYMENT'] )
 
@@ -93,7 +39,7 @@ df['FLAG_OWN_REALTY'] = np.where(((df['FLAG_OWN_REALTY']=='Y') ), '1','0')
 
 df = pd.get_dummies(df, columns=['NAME_CONTRACT_TYPE','WEEKDAY_APPR_PROCESS_START','NAME_TYPE_SUITE',
                                 'NAME_CLIENT_TYPE','NAME_GOODS_CATEGORY','NAME_SELLER_INDUSTRY','NAME_INCOME_TYPE',
-                                 'NAME_EDUCATION_TYPE','NAME_FAMILY_STATUS','NAME_HOUSING_TYPE','NAME_YIELD_GROUP'
+                                 'NAME_EDUCATION_TYPE','NAME_FAMILY_STATUS','NAME_HOUSING_TYPE'
                                 ], drop_first=True)
 
 #prepare datas for build
@@ -101,7 +47,6 @@ selected_features = ['AMT_APPLICATION',
 'AMT_DOWN_PAYMENT',
 'AMT_GOODS_PRICE',
 'HOUR_APPR_PROCESS_START',
-'NAME_CONTRACT_STATUS',
 'CNT_PAYMENT',
 'CODE_GENDER',
 'FLAG_OWN_CAR',
@@ -186,36 +131,20 @@ selected_features = ['AMT_APPLICATION',
 'NAME_HOUSING_TYPE_Municipal apartment',
 'NAME_HOUSING_TYPE_Office apartment',
 'NAME_HOUSING_TYPE_Rented apartment',
-'NAME_HOUSING_TYPE_With parents',
-'NAME_YIELD_GROUP_high',
-'NAME_YIELD_GROUP_low_action',
-'NAME_YIELD_GROUP_low_normal',
-'NAME_YIELD_GROUP_middle']
+'NAME_HOUSING_TYPE_With parents']
+
 
 X = df[selected_features]
 y = df['NAME_CONTRACT_STATUS']
 
+
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size = 0.3, random_state = 100)
 model = DecisionTreeClassifier(criterion = "gini", random_state = 100,
                                max_depth=5, min_samples_leaf=5)
+model.fit(X_train, y_train)
+y_pred_gini = model.predict(X_test)
+pickle.dump(model, open('.../models/loanPrediction.pkl', 'wb') )
 
-model.fit(X, y)
-pickle.dump(model, open('models/loanPrediction.pkl', 'wb') )
-# # Initalize our vectorizer
-# DecisionTree = DecisionTreeClassifier()
 
-# # Fit our vectorizer
-# DecisionTree.fit(X)
-
-# # Transform your X data using your fitted vectorizer. 
-# X = vectorizer.transform(X)
-
-# model = MultinomialNB(alpha=.025)
-
-# # Fit our model with our training data.
-# model.fit(X, y)
-
-# # Save our vectorizer and model.
-# pickle.dump(vectorizer, open('models/vectorizer.pkl', 'wb') )
-# pickle.dump(model, open('models/text-classifier.pkl', 'wb') )
 
 
