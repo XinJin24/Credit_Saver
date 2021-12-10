7341edd6e99bb35453f55232010eec63700f50bb
Xin Jin <jinxin8295@gmail.com>
2021-12-10 09:24:01 -0500

    loan_amount_prediction model added

--- app.py
+++ app.py
@@ -7,22 +7,14 @@ from skimage import transform
 import numpy as np
 from flask import send_from_directory, Flask, request, jsonify, render_template, redirect, url_for
 app = flask.Flask(__name__, template_folder='templates')
-path_to_vectorizer = 'models/vectorizer.pkl'
-path_to_text_classifier = 'models/text-classifier.pkl'
-path_to_image_classifier = 'models/image-classifier.pkl'
+path_to_loan_amount_prediction='models/Loan_Amount_predict.pkl'
 path_to_loan_prediction = 'models/loanPrediction.pkl'
-
-with open(path_to_vectorizer, 'rb') as f:
-    vectorizer = pickle.load(f)
-
-with open(path_to_text_classifier, 'rb') as f:
-    model = pickle.load(f)
-
-with open(path_to_image_classifier, 'rb') as f:
-    image_classifier = pickle.load(f)
 
 with open(path_to_loan_prediction, 'rb') as f:
     loan_prediction = pickle.load(f)
+
+with open(path_to_loan_amount_prediction, 'rb') as f:
+    loan_amount_prediction = pickle.load(f)   
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
@@ -1179,67 +1171,33 @@ def main():
 
     return(flask.render_template('index.html'))
 
-@app.route('/input_values/', methods=['GET', 'POST'])
-def input_values():
+@app.route('/Loan_Amount_predict/', methods=['GET', 'POST'])
+def Loan_Amount_predict():
     if flask.request.method == 'GET':
         # Just render the initial form, to get input
-        return(flask.render_template('input_values.html'))
+        return(flask.render_template('Loan_Amount_predict.html'))
 
     if flask.request.method == 'POST':
         # Get the input from the user.
-        var_one = flask.request.form['input_variable_one']
-        var_two = flask.request.form['another-input-variable']
-        var_three = flask.request.form['third-input-variable']
+        user_credit=flask.request.form['ask_loan_amount']                                
+        user_downPayment=flask.request.form['down_payment_amount']           
+        user_goodsPrice=flask.request.form['goods_price']
+        user_annualIncome=flask.request.form['user_annualIncome']
+        user_daysBirth=float(flask.request.form['user_age'])*-360               
+        user_daysEmployed=float(flask.request.form['User_employment_days'])*-1
+        list_of_inputs=[user_credit,user_downPayment,user_goodsPrice,user_annualIncome,user_daysBirth,user_daysEmployed]
+        list_of_inputs = np.array(list_of_inputs).reshape(1,6)
+        prediction = loan_amount_prediction.predict(list_of_inputs)
+        prediction=round(prediction[0][0],2)
 
-        list_of_inputs = [var_one, var_two, var_three]
+        return (flask.render_template('Loan_Amount_predict.html',amount_result=prediction))
+    return(flask.render_template('Loan_Amount_predict.html'))
 
-        return(flask.render_template('input_values.html', 
-            returned_var_one=var_one,
-            returned_var_two=var_two,
-            returned_var_three=var_three,
-            returned_list=list_of_inputs))
-    return(flask.render_template('input_values.html'))
-
-
-@app.route('/images/')
-def images():
-    return flask.render_template('images.html')
 
 
 @app.route('/bootstrap/')
 def bootstrap():
     return flask.render_template('bootstrap.html')
-
-
-@app.route('/classify_image/', methods=['GET', 'POST'])
-def classify_image():
-    if flask.request.method == 'GET':
-        # Just render the initial form, to get input
-        return(flask.render_template('classify_image.html'))
-
-    if flask.request.method == 'POST':
-        # Get file object from user input.
-        file = flask.request.files['file']
-
-        if file:
-            # Read the image using skimage
-            img = io.imread(file)
-
-            # Resize the image to match the input the model will accept
-            img = transform.resize(img, (28, 28))
-
-            # Flatten the pixels from 28x28 to 784x0
-            img = img.flatten()
-
-            # Get prediction of image from classifier
-            predictions = image_classifier.predict([img])
-
-            # Get the value of the prediction
-            prediction = predictions[0]
-
-            return flask.render_template('classify_image.html', prediction=str(prediction))
-
-    return(flask.render_template('classify_image.html'))
 
 
 if __name__ == '__main__':
