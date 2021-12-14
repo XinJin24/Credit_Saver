6b1320be1776e60f47c96fc0b30b1d77cdfb30cd
Xin Jin <jinxin8295@gmail.com>
2021-12-13 23:02:20 -0500

    project finished

--- create_model.py
+++ create_model.py
@@ -3,148 +3,151 @@ import pickle
 import numpy as np
 from sklearn import tree
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix, f1_score, roc_auc_score 
+from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix, f1_score, roc_auc_score
 from sklearn.model_selection import GridSearchCV
-from sklearn.tree import DecisionTreeClassifier 
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 
-df = pd.read_csv('https://raw.githubusercontent.com/DataScience_Project/Project_Dataset/Application_Data.csv')
+df = pd.read_csv(
+    'https://raw.githubusercontent.com/DataScience_Project/Project_Dataset/Application_Data.csv')
 
-df['NAME_CONTRACT_STATUS']=df['NAME_CONTRACT_STATUS'].replace('Unused offer', 'Approved')
-df.drop(df[df['NAME_CONTRACT_STATUS'] =='Canceled'].index, inplace = True)
+df['NAME_CONTRACT_STATUS'] = df['NAME_CONTRACT_STATUS'].replace(
+    'Unused offer', 'Approved')
+df.drop(df[df['NAME_CONTRACT_STATUS'] == 'Canceled'].index, inplace=True)
 
-df = df.dropna( how='any',subset=['AMT_CREDIT'])
+df = df.dropna(how='any', subset=['AMT_CREDIT'])
 
-df = df.dropna( how='any',subset=['CODE_GENDER'])
+df = df.dropna(how='any', subset=['CODE_GENDER'])
 
-df = df.dropna( how='any',subset=['AMT_GOODS_PRICE'])
+df = df.dropna(how='any', subset=['AMT_GOODS_PRICE'])
 
-df = df.dropna( how='any',subset=['CNT_PAYMENT'])
+df = df.dropna(how='any', subset=['CNT_PAYMENT'])
 
-df = df.dropna( how='any',subset=['CODE_GENDER'])
+df = df.dropna(how='any', subset=['CODE_GENDER'])
 
-df['AMT_DOWN_PAYMENT'] = np.where(((df['AMT_DOWN_PAYMENT'].isnull()==True) ), 0,df['AMT_DOWN_PAYMENT'] )
+df['AMT_DOWN_PAYMENT'] = np.where(
+    ((df['AMT_DOWN_PAYMENT'].isnull() == True)), 0, df['AMT_DOWN_PAYMENT'])
 
-df['OWN_CAR_AGE'] = np.where(((df['OWN_CAR_AGE'].isnull()==True) ), 0,df['OWN_CAR_AGE'] )
+df['OWN_CAR_AGE'] = np.where(
+    ((df['OWN_CAR_AGE'].isnull() == True)), 0, df['OWN_CAR_AGE'])
 
-df['CNT_FAM_MEMBERS'] = np.where(((df['CNT_FAM_MEMBERS'].isnull()==True) ), 0,df['CNT_FAM_MEMBERS'] )
+df['CNT_FAM_MEMBERS'] = np.where(
+    ((df['CNT_FAM_MEMBERS'].isnull() == True)), 0, df['CNT_FAM_MEMBERS'])
 
-df['NAME_TYPE_SUITE'] = np.where(((df['NAME_TYPE_SUITE'].isnull()==True) ), 'No Specified',df['NAME_TYPE_SUITE'] )
+df['NAME_TYPE_SUITE'] = np.where(
+    ((df['NAME_TYPE_SUITE'].isnull() == True)), 'No Specified', df['NAME_TYPE_SUITE'])
 
-df['CODE_GENDER'] = np.where(((df['CODE_GENDER']=='M') ), '1','0')
+df['CODE_GENDER'] = np.where(((df['CODE_GENDER'] == 'M')), '1', '0')
 
-df['FLAG_OWN_CAR'] = np.where(((df['FLAG_OWN_CAR']=='Y') ), '1','0')
+df['FLAG_OWN_CAR'] = np.where(((df['FLAG_OWN_CAR'] == 'Y')), '1', '0')
 
-df['FLAG_OWN_REALTY'] = np.where(((df['FLAG_OWN_REALTY']=='Y') ), '1','0')
+df['FLAG_OWN_REALTY'] = np.where(((df['FLAG_OWN_REALTY'] == 'Y')), '1', '0')
 
-df = pd.get_dummies(df, columns=['NAME_CONTRACT_TYPE','WEEKDAY_APPR_PROCESS_START','NAME_TYPE_SUITE',
-                                'NAME_CLIENT_TYPE','NAME_GOODS_CATEGORY','NAME_SELLER_INDUSTRY','NAME_INCOME_TYPE',
-                                 'NAME_EDUCATION_TYPE','NAME_FAMILY_STATUS','NAME_HOUSING_TYPE'
-                                ], drop_first=True)
+df = pd.get_dummies(df, columns=['NAME_CONTRACT_TYPE', 'WEEKDAY_APPR_PROCESS_START', 'NAME_TYPE_SUITE',
+                                 'NAME_CLIENT_TYPE', 'NAME_GOODS_CATEGORY', 'NAME_SELLER_INDUSTRY', 'NAME_INCOME_TYPE',
+                                 'NAME_EDUCATION_TYPE', 'NAME_FAMILY_STATUS', 'NAME_HOUSING_TYPE'
+                                 ], drop_first=True)
 
-#prepare datas for build
+# prepare datas for build
 selected_features = ['AMT_APPLICATION',
-'AMT_DOWN_PAYMENT',
-'AMT_GOODS_PRICE',
-'HOUR_APPR_PROCESS_START',
-'CNT_PAYMENT',
-'CODE_GENDER',
-'FLAG_OWN_CAR',
-'FLAG_OWN_REALTY',
-'CNT_CHILDREN',
-'AMT_INCOME_TOTAL',
-'DAYS_BIRTH',
-'DAYS_EMPLOYED',
-'OWN_CAR_AGE',
-'FLAG_MOBIL',
-'FLAG_EMAIL',
-'CNT_FAM_MEMBERS',
-'NAME_CONTRACT_TYPE_Consumer loans',
-'NAME_CONTRACT_TYPE_Revolving loans',
-'WEEKDAY_APPR_PROCESS_START_MONDAY',
-'WEEKDAY_APPR_PROCESS_START_SATURDAY',
-'WEEKDAY_APPR_PROCESS_START_SUNDAY',
-'WEEKDAY_APPR_PROCESS_START_THURSDAY',
-'WEEKDAY_APPR_PROCESS_START_TUESDAY',
-'WEEKDAY_APPR_PROCESS_START_WEDNESDAY',
-'NAME_TYPE_SUITE_Family',
-'NAME_TYPE_SUITE_Group of people',
-'NAME_TYPE_SUITE_No Specified',
-'NAME_TYPE_SUITE_Other_A',
-'NAME_TYPE_SUITE_Other_B',
-'NAME_TYPE_SUITE_Spouse, partner',
-'NAME_TYPE_SUITE_Unaccompanied',
-'NAME_CLIENT_TYPE_Refreshed',
-'NAME_CLIENT_TYPE_Repeater',
-'NAME_CLIENT_TYPE_XNA',
-'NAME_GOODS_CATEGORY_Animals',
-'NAME_GOODS_CATEGORY_Audio/Video',
-'NAME_GOODS_CATEGORY_Auto Accessories',
-'NAME_GOODS_CATEGORY_Clothing and Accessories',
-'NAME_GOODS_CATEGORY_Computers',
-'NAME_GOODS_CATEGORY_Construction Materials',
-'NAME_GOODS_CATEGORY_Consumer Electronics',
-'NAME_GOODS_CATEGORY_Direct Sales',
-'NAME_GOODS_CATEGORY_Education',
-'NAME_GOODS_CATEGORY_Fitness',
-'NAME_GOODS_CATEGORY_Furniture',
-'NAME_GOODS_CATEGORY_Gardening',
-'NAME_GOODS_CATEGORY_Homewares',
-'NAME_GOODS_CATEGORY_Insurance',
-'NAME_GOODS_CATEGORY_Jewelry',
-'NAME_GOODS_CATEGORY_Medical Supplies',
-'NAME_GOODS_CATEGORY_Medicine',
-'NAME_GOODS_CATEGORY_Mobile',
-'NAME_GOODS_CATEGORY_Office Appliances',
-'NAME_GOODS_CATEGORY_Other',
-'NAME_GOODS_CATEGORY_Photo / Cinema Equipment',
-'NAME_GOODS_CATEGORY_Sport and Leisure',
-'NAME_GOODS_CATEGORY_Tourism',
-'NAME_GOODS_CATEGORY_Vehicles',
-'NAME_GOODS_CATEGORY_Weapon',
-'NAME_GOODS_CATEGORY_XNA',
-'NAME_SELLER_INDUSTRY_Clothing',
-'NAME_SELLER_INDUSTRY_Connectivity',
-'NAME_SELLER_INDUSTRY_Construction',
-'NAME_SELLER_INDUSTRY_Consumer electronics',
-'NAME_SELLER_INDUSTRY_Furniture',
-'NAME_SELLER_INDUSTRY_Industry',
-'NAME_SELLER_INDUSTRY_Jewelry',
-'NAME_SELLER_INDUSTRY_MLM partners',
-'NAME_SELLER_INDUSTRY_Tourism',
-'NAME_SELLER_INDUSTRY_XNA',
-'NAME_INCOME_TYPE_Maternity leave',
-'NAME_INCOME_TYPE_Pensioner',
-'NAME_INCOME_TYPE_State servant',
-'NAME_INCOME_TYPE_Student',
-'NAME_INCOME_TYPE_Unemployed',
-'NAME_INCOME_TYPE_Working',
-'NAME_EDUCATION_TYPE_Higher education',
-'NAME_EDUCATION_TYPE_Incomplete higher',
-'NAME_EDUCATION_TYPE_Lower secondary',
-'NAME_EDUCATION_TYPE_Secondary / secondary special',
-'NAME_FAMILY_STATUS_Married',
-'NAME_FAMILY_STATUS_Separated',
-'NAME_FAMILY_STATUS_Single / not married',
-'NAME_FAMILY_STATUS_Widow',
-'NAME_HOUSING_TYPE_House / apartment',
-'NAME_HOUSING_TYPE_Municipal apartment',
-'NAME_HOUSING_TYPE_Office apartment',
-'NAME_HOUSING_TYPE_Rented apartment',
-'NAME_HOUSING_TYPE_With parents']
+                     'AMT_DOWN_PAYMENT',
+                     'AMT_GOODS_PRICE',
+                     'HOUR_APPR_PROCESS_START',
+                     'CNT_PAYMENT',
+                     'CODE_GENDER',
+                     'FLAG_OWN_CAR',
+                     'FLAG_OWN_REALTY',
+                     'CNT_CHILDREN',
+                     'AMT_INCOME_TOTAL',
+                     'DAYS_BIRTH',
+                     'DAYS_EMPLOYED',
+                     'OWN_CAR_AGE',
+                     'FLAG_MOBIL',
+                     'FLAG_EMAIL',
+                     'CNT_FAM_MEMBERS',
+                     'NAME_CONTRACT_TYPE_Consumer loans',
+                     'NAME_CONTRACT_TYPE_Revolving loans',
+                     'WEEKDAY_APPR_PROCESS_START_MONDAY',
+                     'WEEKDAY_APPR_PROCESS_START_SATURDAY',
+                     'WEEKDAY_APPR_PROCESS_START_SUNDAY',
+                     'WEEKDAY_APPR_PROCESS_START_THURSDAY',
+                     'WEEKDAY_APPR_PROCESS_START_TUESDAY',
+                     'WEEKDAY_APPR_PROCESS_START_WEDNESDAY',
+                     'NAME_TYPE_SUITE_Family',
+                     'NAME_TYPE_SUITE_Group of people',
+                     'NAME_TYPE_SUITE_No Specified',
+                     'NAME_TYPE_SUITE_Other_A',
+                     'NAME_TYPE_SUITE_Other_B',
+                     'NAME_TYPE_SUITE_Spouse, partner',
+                     'NAME_TYPE_SUITE_Unaccompanied',
+                     'NAME_CLIENT_TYPE_Refreshed',
+                     'NAME_CLIENT_TYPE_Repeater',
+                     'NAME_CLIENT_TYPE_XNA',
+                     'NAME_GOODS_CATEGORY_Animals',
+                     'NAME_GOODS_CATEGORY_Audio/Video',
+                     'NAME_GOODS_CATEGORY_Auto Accessories',
+                     'NAME_GOODS_CATEGORY_Clothing and Accessories',
+                     'NAME_GOODS_CATEGORY_Computers',
+                     'NAME_GOODS_CATEGORY_Construction Materials',
+                     'NAME_GOODS_CATEGORY_Consumer Electronics',
+                     'NAME_GOODS_CATEGORY_Direct Sales',
+                     'NAME_GOODS_CATEGORY_Education',
+                     'NAME_GOODS_CATEGORY_Fitness',
+                     'NAME_GOODS_CATEGORY_Furniture',
+                     'NAME_GOODS_CATEGORY_Gardening',
+                     'NAME_GOODS_CATEGORY_Homewares',
+                     'NAME_GOODS_CATEGORY_Insurance',
+                     'NAME_GOODS_CATEGORY_Jewelry',
+                     'NAME_GOODS_CATEGORY_Medical Supplies',
+                     'NAME_GOODS_CATEGORY_Medicine',
+                     'NAME_GOODS_CATEGORY_Mobile',
+                     'NAME_GOODS_CATEGORY_Office Appliances',
+                     'NAME_GOODS_CATEGORY_Other',
+                     'NAME_GOODS_CATEGORY_Photo / Cinema Equipment',
+                     'NAME_GOODS_CATEGORY_Sport and Leisure',
+                     'NAME_GOODS_CATEGORY_Tourism',
+                     'NAME_GOODS_CATEGORY_Vehicles',
+                     'NAME_GOODS_CATEGORY_Weapon',
+                     'NAME_GOODS_CATEGORY_XNA',
+                     'NAME_SELLER_INDUSTRY_Clothing',
+                     'NAME_SELLER_INDUSTRY_Connectivity',
+                     'NAME_SELLER_INDUSTRY_Construction',
+                     'NAME_SELLER_INDUSTRY_Consumer electronics',
+                     'NAME_SELLER_INDUSTRY_Furniture',
+                     'NAME_SELLER_INDUSTRY_Industry',
+                     'NAME_SELLER_INDUSTRY_Jewelry',
+                     'NAME_SELLER_INDUSTRY_MLM partners',
+                     'NAME_SELLER_INDUSTRY_Tourism',
+                     'NAME_SELLER_INDUSTRY_XNA',
+                     'NAME_INCOME_TYPE_Maternity leave',
+                     'NAME_INCOME_TYPE_Pensioner',
+                     'NAME_INCOME_TYPE_State servant',S
+                     'NAME_INCOME_TYPE_Student',
+                     'NAME_INCOME_TYPE_Unemployed',
+                     'NAME_INCOME_TYPE_Working',
+                     'NAME_EDUCATION_TYPE_Higher education',
+                     'NAME_EDUCATION_TYPE_Incomplete higher',
+                     'NAME_EDUCATION_TYPE_Lower secondary',
+                     'NAME_EDUCATION_TYPE_Secondary / secondary special',
+                     'NAME_FAMILY_STATUS_Married',
+                     'NAME_FAMILY_STATUS_Separated',
+                     'NAME_FAMILY_STATUS_Single / not married',
+                     'NAME_FAMILY_STATUS_Widow',
+                     'NAME_HOUSING_TYPE_House / apartment',
+                     'NAME_HOUSING_TYPE_Municipal apartment',
+                     'NAME_HOUSING_TYPE_Office apartment',
+                     'NAME_HOUSING_TYPE_Rented apartment',
+                     'NAME_HOUSING_TYPE_With parents']
 
 
 X = df[selected_features]
 y = df['NAME_CONTRACT_STATUS']
 
 
-X_train, X_test, y_train, y_test = train_test_split( X, y, test_size = 0.3, random_state = 100)
-model = DecisionTreeClassifier(criterion = "gini", random_state = 100,
-                               max_depth=5, min_samples_leaf=5)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=100)
+model = DecisionTreeClassifier(criterion="gini", random_state=100,
+                               max_depth=9, min_samples_leaf=9)
 model.fit(X_train, y_train)
 y_pred_gini = model.predict(X_test)
-pickle.dump(model, open('.../models/loanPrediction.pkl', 'wb') )
-
-
-
-
+pickle.dump(model, open('.../models/loanPrediction.pkl', 'wb'))
--- maximum_loan_amount_model.py
+++ maximum_loan_amount_model.py
@@ -1,78 +1,83 @@
-# Import our libraries 
+# Import our libraries
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix, f1_score, roc_auc_score
+import scipy.stats as stats
+from sklearn import metrics
+from sklearn.linear_model import LinearRegression
+import graphviz
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn import tree
+import matplotlib.pyplot as plt
 import pickle
 # Pandas and numpy for data wrangling
 import pandas as pd
 import numpy as np
-# Seaborn / matplotlib for visualization 
+# Seaborn / matplotlib for visualization
 import seaborn as sns
 sns.set()
-import matplotlib.pyplot as plt
 # Import the trees from sklearn
-from sklearn import tree
 
 # Helper function to split our data
-from sklearn.model_selection import train_test_split
 
 # Helper fuctions to evaluate our model.
-from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix, f1_score, roc_auc_score 
 
 # Helper function for hyper-parameter turning.
-from sklearn.model_selection import GridSearchCV
 
 # Import our Decision Tree
-from sklearn.tree import DecisionTreeClassifier 
 
-# Import our Random Forest 
-from sklearn.ensemble import RandomForestClassifier
+# Import our Random Forest
 
 # Library for visualizing our tree
 # If you get an error, run 'conda install python-graphviz' in your terminal
-import graphviz
-from sklearn.linear_model import LinearRegression
-from sklearn import metrics
-import scipy.stats as stats
 
 
-df = pd.read_csv('https://raw.githubusercontent.com/DataScience_Project/Project_Dataset/Application_Data.csv')
+df = pd.read_csv(
+    'https://raw.githubusercontent.com/DataScience_Project/Project_Dataset/Application_Data.csv')
 # df = pd.read_csv('Project_Dataset/Application_Data.csv')
-df['NAME_CONTRACT_STATUS']=df['NAME_CONTRACT_STATUS'].replace('Unused offer', 'Approved')
+df['NAME_CONTRACT_STATUS'] = df['NAME_CONTRACT_STATUS'].replace(
+    'Unused offer', 'Approved')
 
 # df.drop(df[df['NAME_CONTRACT_STATUS'] =='Canceled'].index, inplace = True)
 
-df = df.dropna( how='any',subset=['AMT_CREDIT'])
+df = df.dropna(how='any', subset=['AMT_CREDIT'])
 
-df = df.dropna( how='any',subset=['CODE_GENDER'])
+df = df.dropna(how='any', subset=['CODE_GENDER'])
 
-df = df.dropna( how='any',subset=['AMT_GOODS_PRICE'])
+df = df.dropna(how='any', subset=['AMT_GOODS_PRICE'])
 
-df = df.dropna( how='any',subset=['CNT_PAYMENT'])
+df = df.dropna(how='any', subset=['CNT_PAYMENT'])
 
-df = df.dropna( how='any',subset=['CODE_GENDER'])
+df = df.dropna(how='any', subset=['CODE_GENDER'])
 
-df['AMT_DOWN_PAYMENT'] = np.where(((df['AMT_DOWN_PAYMENT'].isnull()==True) ), 0,df['AMT_DOWN_PAYMENT'] )
+df['AMT_DOWN_PAYMENT'] = np.where(
+    ((df['AMT_DOWN_PAYMENT'].isnull() == True)), 0, df['AMT_DOWN_PAYMENT'])
 
-df['OWN_CAR_AGE'] = np.where(((df['OWN_CAR_AGE'].isnull()==True) ), 0,df['OWN_CAR_AGE'] )
+df['OWN_CAR_AGE'] = np.where(
+    ((df['OWN_CAR_AGE'].isnull() == True)), 0, df['OWN_CAR_AGE'])
 
-df['CNT_FAM_MEMBERS'] = np.where(((df['CNT_FAM_MEMBERS'].isnull()==True) ), 0,df['CNT_FAM_MEMBERS'] )
+df['CNT_FAM_MEMBERS'] = np.where(
+    ((df['CNT_FAM_MEMBERS'].isnull() == True)), 0, df['CNT_FAM_MEMBERS'])
 
-df['NAME_TYPE_SUITE'] = np.where(((df['NAME_TYPE_SUITE'].isnull()==True) ), 'No Specified',df['NAME_TYPE_SUITE'] )
+df['NAME_TYPE_SUITE'] = np.where(
+    ((df['NAME_TYPE_SUITE'].isnull() == True)), 'No Specified', df['NAME_TYPE_SUITE'])
 
-df['CODE_GENDER'] = np.where(((df['CODE_GENDER']=='M') ), '1','0')
+df['CODE_GENDER'] = np.where(((df['CODE_GENDER'] == 'M')), '1', '0')
 
-df['FLAG_OWN_CAR'] = np.where(((df['FLAG_OWN_CAR']=='Y') ), '1','0')
+df['FLAG_OWN_CAR'] = np.where(((df['FLAG_OWN_CAR'] == 'Y')), '1', '0')
 
-df['FLAG_OWN_REALTY'] = np.where(((df['FLAG_OWN_REALTY']=='Y') ), '1','0')
+df['FLAG_OWN_REALTY'] = np.where(((df['FLAG_OWN_REALTY'] == 'Y')), '1', '0')
 
-df.drop(['NAME_CONTRACT_TYPE','WEEKDAY_APPR_PROCESS_START','HOUR_APPR_PROCESS_START','NAME_TYPE_SUITE','NAME_CLIENT_TYPE',
-        'NAME_GOODS_CATEGORY','NAME_SELLER_INDUSTRY','NAME_INCOME_TYPE','NAME_EDUCATION_TYPE','NAME_FAMILY_STATUS','NAME_HOUSING_TYPE',], axis=1, inplace=True)
-#prepare datas for build
-
+df.drop(['NAME_CONTRACT_TYPE', 'WEEKDAY_APPR_PROCESS_START', 'HOUR_APPR_PROCESS_START', 'NAME_TYPE_SUITE', 'NAME_CLIENT_TYPE',
+        'NAME_GOODS_CATEGORY', 'NAME_SELLER_INDUSTRY', 'NAME_INCOME_TYPE', 'NAME_EDUCATION_TYPE', 'NAME_FAMILY_STATUS', 'NAME_HOUSING_TYPE', ], axis=1, inplace=True)
+# prepare datas for build
 
 
 original_cols = df.columns
 
-target_cols = ['AMT_APPLICATION','AMT_DOWN_PAYMENT','AMT_GOODS_PRICE',
-               'CNT_PAYMENT','AMT_INCOME_TOTAL','DAYS_BIRTH','DAYS_EMPLOYED']
+target_cols = [ 'AMT_DOWN_PAYMENT', 'AMT_GOODS_PRICE',
+               'CNT_PAYMENT', 'AMT_INCOME_TOTAL', 'DAYS_BIRTH', 'DAYS_EMPLOYED']
 
 z_score_cols = []
 
@@ -80,13 +85,13 @@ z_score_cols = []
 for col in target_cols:
     # Make the new column name the same as the original but with 'z_score' added to it
     new_col_name = col + "_zscore"
-    
+
     # Set the new column equal to the score
-    df[new_col_name] = stats.stats.zscore( df[col] )
-    
+    df[new_col_name] = stats.stats.zscore(df[col])
+
     # Set the z-score to its absolute value of the for easier filtering
-    df[new_col_name] = abs( df[new_col_name] )
-    
+    df[new_col_name] = abs(df[new_col_name])
+
     # Append the new column name our our z_score_cols list for easier access for later.
     z_score_cols.append(new_col_name)
 
@@ -104,8 +109,8 @@ df = df[condition]
 print('After removal of outliers', df.shape)
 
 
-
-features = ['AMT_APPLICATION','AMT_DOWN_PAYMENT','AMT_GOODS_PRICE','AMT_INCOME_TOTAL','DAYS_BIRTH','DAYS_EMPLOYED']
+features = ['AMT_DOWN_PAYMENT', 'AMT_GOODS_PRICE',
+            'AMT_INCOME_TOTAL', 'DAYS_BIRTH', 'DAYS_EMPLOYED']
 target = ['AMT_CREDIT']
 
 X = df[features].values
@@ -113,7 +118,8 @@ X = df[features].values
 
 # ISOLATE JUST OUR TARGET DATA, THIS IS WHAT WE ARE TRYING TO PREDICT
 y = df[target].values
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=1)
 model = LinearRegression()
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
@@ -133,8 +139,4 @@ df_coefficients = pd.DataFrame(coefficient_values, columns=features).T
 df_coefficients.columns = ['coefficient']
 
 y_pred = model.predict(X_test)
-pickle.dump(model, open('.../models/Loan_Amount_predict.pkl', 'wb') )
-
-
-
-
+pickle.dump(model, open('.../models/Loan_Amount_predict.pkl', 'wb'))
